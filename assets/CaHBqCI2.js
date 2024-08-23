8397976ce292939268baaf49690a04eedf3e8812
Jean-Martin Allaire <jeanmartinallaire@gmail.com>
2024-08-23 01:08:53 -0400

    fixing menu issue on mobile

--- assets/CaHBqCI2.js
+++
@@ -1,1 +0,0 @@
-import{C as e,n as o}from"./CKOS0k2r.js";const n=e((a,t)=>{console.log("into middleware"),o("/Games/")});export{n as default};
--- assets/DFWArqVf.js
+++
@@ -1,1 +0,0 @@
-import{p as s}from"./CKOS0k2r.js";const p=s("/unevaguealafois.png");export{p as _};
