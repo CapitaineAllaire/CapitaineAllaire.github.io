eb5ecce8fccc5df4e6c6d997fd3bc6793b1b5124
Jean-Martin Allaire <jeanmartinallaire@gmail.com>
2024-08-25 14:06:18 -0400

    add validation on location form.

--- assets/2GKZMhgO.js
+++
@@ -1,1 +0,0 @@
-import{p as s}from"./B_LhBNBI.js";const p=s("/unevaguealafois.png");export{p as _};
--- assets/C_0Xu7X3.js
+++
@@ -1,1 +0,0 @@
-import{B as e,n as o}from"./B_LhBNBI.js";const n=e((a,t)=>{console.log("into middleware"),o("/Games/")});export{n as default};
