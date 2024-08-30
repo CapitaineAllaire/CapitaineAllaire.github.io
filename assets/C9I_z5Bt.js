d7e1b7d639ddd14401a25fe732d530a1c57ae5d5
Jean-Martin Allaire <jeanmartinallaire@gmail.com>
2024-08-29 22:45:20 -0400

    fix spelling error

---
+++ assets/C9I_z5Bt.js
@@ -0,0 +1,1 @@
+import{B as e,n as o}from"./Bq1pCMbV.js";const n=e((a,t)=>{console.log("into middleware"),o("/Games/")});export{n as default};
---
+++ assets/CCWaSzW0.js
@@ -0,0 +1,1 @@
+import{c as e,o,a as t}from"./Bq1pCMbV.js";const s=t("div",null,"Choisir votre version:",-1),n=[s],i={__name:"Download",setup(a){return(c,r)=>(o(),e("section",null,n))}};export{i as default};
