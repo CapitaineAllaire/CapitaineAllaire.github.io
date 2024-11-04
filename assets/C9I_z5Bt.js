cc401339dac379bf4e00d30f869b607e55df3afb
Jean-Martin Allaire <jeanmartinallaire@gmail.com>
2024-11-04 07:22:02 -0500

    remove fake prices and fix text errors

--- assets/C9I_z5Bt.js
+++
@@ -1,1 +0,0 @@
-import{B as e,n as o}from"./Bq1pCMbV.js";const n=e((a,t)=>{console.log("into middleware"),o("/Games/")});export{n as default};
--- assets/CCWaSzW0.js
+++
@@ -1,1 +0,0 @@
-import{c as e,o,a as t}from"./Bq1pCMbV.js";const s=t("div",null,"Choisir votre version:",-1),n=[s],i={__name:"Download",setup(a){return(c,r)=>(o(),e("section",null,n))}};export{i as default};
