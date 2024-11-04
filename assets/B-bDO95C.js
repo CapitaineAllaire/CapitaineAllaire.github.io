dc34a270793549b7d135a7126b40d4ad3132cce4
Jean-Martin Allaire <jeanmartinallaire@gmail.com>
2024-11-04 08:59:07 -0500

    add scuba diving option to location

--- assets/B-bDO95C.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c as t,o}from"./559iMjW3.js";const n={},c={class:"text-3xl font-bold"};function r(s,i){return o(),t("div",c,"Soyez patient! Bientôt Notre Petite mine d'or de produits divers!")}const _=e(n,[["render",r]]);export{_ as default};
--- assets/BKdLwgCj.js
+++
@@ -1,1 +0,0 @@
-import{B as e,n as o}from"./559iMjW3.js";const n=e((a,t)=>{console.log("into middleware"),o("/Games/")});export{n as default};
